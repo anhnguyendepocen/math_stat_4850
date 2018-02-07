e683f337e12f6e0e1a0058e1bc5cb7c0b88c23b7
Darrin Speegle <speegled@slu.edu>
2018-02-07 10:53:04 -0600

    changes from class

--- bootstrap/february_7.R
+++ bootstrap/february_7.R
@@ -1,6 +1,8 @@
 library(resampledata)
 
 wts <- NCBirths2004$Weight
+hist(wts)
+length(wts)
 bootstrap_sample_dist <- replicate(20000, {
   wts2 <- wts[sample(length(wts), length(wts), TRUE)]
   mean(wts2)
@@ -12,4 +14,9 @@ U <- quantile(bootstrap_sample_dist, .975) #Upper 95% CI bound
 M <- mean(bootstrap_sample_dist)
 
 abline(v = c(L, U, M), col = "red", lty = 2)
-
+#3418 - 3478 is the 95% confidence interval of baby weights
+t.test(wts)
+L
+U
+ars <- MnGroundwater$Arsenic
+hist(ars)
